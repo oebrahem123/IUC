08b9d5c53e07a858d145dfa80e50586a3f2a51d7
Omar Ebrahem <your_email@example.com>
2026-02-25 00:34:33 +0200

    update IUC animations and performance

--- style/js/script.js
+++ style/js/script.js
@@ -132,3 +132,18 @@ function validateEmail(email) {
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 }
 // End Login Page Script
+// Start Animation 
+// Search
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    }
+  });
+});
